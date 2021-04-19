0f4c3e0eaae1e17a1a199028d8f038546d46457c
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-19 05:50:28 +0200

    making the code leaner

--- ArrayHelpers/reduce/tally.js
+++ ArrayHelpers/reduce/tally.js
@@ -1,12 +1,23 @@
 const votes = ['y', 'n', 'n', 'y', 'y', 'n', 'n', 'y', 'y', 'n', 'n', 'y', 'y', 'n', 'n', 'y'];
 
+
 const results = votes.reduce((tally, currentVote) => {
-    if (tally[currentVote]) {
-        tally[currentVote]++
-    } else {
-        tally[currentVote] = 1;
-    }
+    tally[currentVote] = (tally[currentVote] || 0) + 1
     return tally
 }, {})
 
+
 console.log(results);
+
+// tally[val] = (undefined || 0) + 1 becomes 1
+// tally[val] = (1 || 0) + 1 becomes 2
+// tally[val] = (2 || 0) + 1 becomes 3
+
+// const results = votes.reduce((tally, currentVote) => {
+//     if (tally[currentVote]) {
+//         tally[currentVote]++
+//     } else {
+//         tally[currentVote] = 1;
+//     }
+//     return tally
+// }, {})

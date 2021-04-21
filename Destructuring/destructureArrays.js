b1e0767619f7c7cbd4a49b1c4738164d1e31fc85
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-21 19:39:58 +0200

    learning to destructure arrays

---
+++ Destructuring/destructureArrays.js
@@ -0,0 +1,9 @@
+const myProgramming = ['react', 'angular', 'express', 'nest', 'flutter', 'javascript', 'typescript', 'java', 'swift', 'dart', 'c#', 'python', 'selenium', 'protractor', 'jest', 'puppeteer'];
+
+const [strongest, experienced, dearest, gateway, , , , firstProgrammingLanguage, awakening, mvpl, paidBills, firstUsefulTool, ...testAutomation] = myProgramming;
+
+console.log(`I started coding by learning ${firstProgrammingLanguage}, but I never got deep into it. ${strongest} is my strongest framework. I started being aware whiles learning ${awakening}. However I have more professional experience with ${experienced}.
+
+${dearest} holds a special place in my heart as it taught me backend, however ${gateway} will teach me the design patterns used in the industry. I think ${mvpl} will be my most valuable programming language
+Lets never forget that ${paidBills} paid the bills for almost a year but I worte my first functioning tool in ${firstUsefulTool}. I can test what i build using ${testAutomation}
+`);

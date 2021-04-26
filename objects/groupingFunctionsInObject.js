b82c5ae2530d8697a7355f779f408801ea529ab6
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-26 07:25:25 +0200

    adding modern syntax to objects with a group of methods

--- objects/groupingFunctionsInObject.js
+++ objects/groupingFunctionsInObject.js
@@ -16,6 +16,30 @@ const arithmetic = {
     }
 }
 
+const arithmeticModern = {
+    add(x, y) {
+        return x + y
+    },
+    multiply(x, y) {
+        return x * y;
+    },
+    subtract(x, y) {
+        return x - y;
+    },
+    divide(x, y) {
+        return x / y;
+    },
+    remainder(x, y) {
+        return x % y
+    }
+}
+
+console.log(arithmeticModern.add(6, 5));
+console.log(arithmeticModern.subtract(1, 0));
+console.log(arithmeticModern.divide(8, 4));
+console.log(arithmeticModern.multiply(2, 3));
+console.log(arithmeticModern.remainder(9, 7));
+
 console.log(arithmetic.add(6, 5));
 console.log(arithmetic.subtract(1, 0));
 console.log(arithmetic.divide(8, 4));

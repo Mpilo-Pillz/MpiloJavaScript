a7410503ccc2c4028e028938c20d4a64278e17db
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-27 20:56:34 +0200

    adding method to object to show how the value of this differs

--- This/learningThis.js
+++ This/learningThis.js
@@ -1,7 +1,7 @@
 /* when the below function is executed in the browser th evalue of
 this will be th ewindow object and we will find the sayHi function as 
 a property in the window object.
-
+this refers to the current execution scope
 var get added to the global scope of window but let and const do not
 */
 function sayHi() {
@@ -12,4 +12,24 @@ function sayHi() {
 const greet = function () {
     //this will still refer to the window object
     console.log(this);
+}
+
+const person = {
+    first: 'Cherilyn',
+    last: 'Sarkisian',
+    nickname: 'Cher',
+    fullName() {
+        //this in this case is the object itself
+        console.log(this);
+        console.log(`${this.first} ${this.last} AKA ${this.nickname}`);
+    },
+    fullNameDestructure() {
+        //this in this case is the object itself
+        const { first, last, nickname } = this;
+        return `${first} ${last} AKA ${nickname}`;
+    },
+    printBio() {
+        const fullName = this.fullNameDestructure();
+        console.log(`${fullName} is a person`);
+    }
 }

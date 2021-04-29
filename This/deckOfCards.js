96449713bdbfa22abd02253f87e5e51be99a598a
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-29 21:33:38 +0200

    refactoring to use objects, properties and methods

--- This/deckOfCards.js
+++ This/deckOfCards.js
@@ -15,5 +15,33 @@ function drawCard(deck) {
     return deck.pop()
 }
 
-const myDeck = makeDeck();
-const card1 = drawCard(myDeck);
+// const myDeck = makeDeck();
+// const card1 = drawCard(myDeck);
+
+const myDeck = {
+    deck: [],
+    drawnCards: [],
+    suits: ['hearts', 'diamonds', 'spades', 'clubs'],
+    values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+    initializeDeck() {
+        const { suits, values, deck } = this;
+        for (let value of values.split(',')) {
+            for (let suit of suits) {
+                deck.push({ value, fuit })
+            }
+        }
+        // return deck;
+    },
+    drawCard() {
+        const card = this.deck.pop()
+        this.drawCard.push(card)
+        return card;
+    },
+    drawMultiple(numCards) {
+        const cards = [];
+        for (let i = 0; i < numCards; i++) {
+            cards.push(this.drawCard());
+        }
+        return cards;
+    }
+}

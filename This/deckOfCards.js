dde9086d873307562657f4b8f30e8d53f51ee74c
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-30 07:55:36 +0200

    showing that using object is a better way of organising code with the deckOfCards

--- This/deckOfCards.js
+++ This/deckOfCards.js
@@ -15,6 +15,13 @@ function drawCard(deck) {
     return deck.pop()
 }
 
+const firstDeck = initializeDeck()
+const drawnCards = []
+shuffle(firstDeck)
+const hand1 = drawMultipleForPlayer(5, firstDeck, drawnCards);
+const hand2 = drawnCards(5, firstDeck, drawnCards);
+const pokerHand = drawMultipleForPlayer(5, firstDeck, drawnCards)
+
 // const myDeck = makeDeck();
 // const card1 = drawCard(myDeck);
 
@@ -43,5 +50,38 @@ const myDeck = {
             cards.push(this.drawCard());
         }
         return cards;
+    },
+    drawMultipleForPlayer(numCards, deck, drawCards) {
+        const cards = [];
+        for (let i = 0; i < numCards; i++) {
+            cards.push(drawCard(deck, this.drawnCards));
+        }
+        return cards;
+    },
+    shuffle() {
+        const [deck] = this;
+        for (let i = deck.length - 1; i > 0; i--) {
+            let j = Math.floor.random() * (i + 1);
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+            console.log(deck);
+        }
     }
 }
+
+myDeck.initializeDeck();
+myDeck.shuffle();
+const h1 = myDeck.drawMultiple(5);
+const h3 = myDeck.drawMultiple(6);
+const h2 = myDeck.drawMultiple(7);
+
+// function shuffle(arr) {
+//     // loop over array backwards
+//     for (let i = arr.length - 1; i > 0; i--) {
+//         //pick random index before current element
+//         let j = Math.floor.random() * (i + 1);
+//         //swap
+//         // [letters[0], letters[3]] = [letters[3], letters[0]]
+//         [arr[i], arr[j]] = [arr[j], arr[i]];
+//         console.log(arr);
+//     }
+// }

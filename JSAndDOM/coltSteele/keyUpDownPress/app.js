109444a9c3cf021dd89751e637add7c0f15eed14
Fikasentani <mpilo.dlamini@yahoo.com>
2021-06-07 06:21:57 +0200

    adding shopping list implimenting key events

--- JSAndDOM/coltSteele/keyUpDownPress/app.js
+++ JSAndDOM/coltSteele/keyUpDownPress/app.js
@@ -14,3 +14,23 @@ username.addEventListener('keypress', function (e) {
     console.log("KEY PRESS -may differ form browser to browser");
 });
 
+const addItemInput = document.querySelector('#addITem');
+const itemsUL = document.getElementById('items');
+
+addItemInput.addEventListener('keypress', function (e) {
+    console.log('EVENT OBJECT-->', e);
+    if (e.key === 'Enter') {
+        if (!this.value) {
+            itemsUL.insertAdjacentHTML('beforebegin', '<p><em>enter something</em></p>')
+            return;
+        };
+        //add new item to list
+        console.log(this.value);
+        const newItemText = this.value;
+        const newItem = document.createElement('li');
+        newItem.innerText = newItemText;
+        itemsUL.appendChild(newItem);
+        this.value = '';
+    }
+})
+

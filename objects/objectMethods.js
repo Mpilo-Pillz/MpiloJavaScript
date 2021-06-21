8dd6df8dc53780e8e7228cc5304a73bfd5694d43
mpilopillz <mpilo.dlamini@yahoo.com>
2021-06-21 21:29:10 +0200

    learning object dot entries

--- objects/objectMethods.js
+++ objects/objectMethods.js
@@ -1,24 +1,34 @@
 let restaurant = {
-    name: 'Dla-Mini Bistro',
-    guestCapacity: 100,
-    guestCount: 0,
-    checkAvailability: function(partySize) {
-        // console.log(this);
-        let seatsLeft = this.guestCapacity - this.guestCount
-        return partySize <= seatsLeft;
-    },
-    seatParty: function(partySize) {
-        this.guestCount = this.guestCount + partySize;
-    },
-    removeParty: function(partySize) {
-        this.guestCount = this.guestCount - partySize;
-    },
+  name: "Dla-Mini Bistro",
+  guestCapacity: 100,
+  guestCount: 0,
+  checkAvailability: function (partySize) {
+    // console.log(this);
+    let seatsLeft = this.guestCapacity - this.guestCount;
+    return partySize <= seatsLeft;
+  },
+  seatParty: function (partySize) {
+    this.guestCount = this.guestCount + partySize;
+  },
+  removeParty: function (partySize) {
+    this.guestCount = this.guestCount - partySize;
+  },
+};
 
-    
+restaurant.seatParty(85);
+console.log(restaurant.checkAvailability(20));
+restaurant.removeParty(70);
+console.log(restaurant.checkAvailability(10));
+// console.log(status);
+
+const object1 = {
+  a: "somestring",
+  b: 42,
+  c: true,
+};
+
+for (const [key, value] of Object.entries(object1)) {
+  console.log(`${key}: ${value}`);
 }
 
-restaurant.seatParty(85)
-console.log(restaurant.checkAvailability(20)); 
-restaurant.removeParty(70);
-console.log(restaurant.checkAvailability(10)); 
-// console.log(status);
+console.log(Object.entries(object1));

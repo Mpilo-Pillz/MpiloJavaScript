ca8332d6f55dc2ce7af78d7771a52d4f4f735a81
mpilopillz <mpilo.dlamini@yahoo.com>
2021-06-23 04:50:30 +0200

    wrapping delayed promise in a function

--- AsynchronousJS/callBackHeck/promiseExampleWithDelay.js
+++ AsynchronousJS/callBackHeck/promiseExampleWithDelay.js
@@ -1,18 +1,21 @@
-const keepingPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    const rand = Math.random();
+const keepingPromise = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const rand = Math.random();
 
-    if (rand < 0.5) {
-      resolve("resolves the promise with the value inside");
-    } else {
-      reject(
-        "if yo dont catch it will complain about uncaught in promise. TODO: SEE IF THAT CAN BE CAUGTH"
-      );
-    }
-  }, 5000);
+      if (rand < 0.5) {
+        resolve("resolves the promise with the value inside");
+      } else {
+        reject(
+          "if yo dont catch it will complain about uncaught in promise. TODO: SEE IF THAT CAN BE CAUGTH"
+        );
+      }
+    }, 5000);
 
-  //if you do not do any of the above the promise will be stuck on pending
-});
+    //if you do not do any of the above the promise will be stuck on pending
+  });
+};
 
-keepingPromise.then((data) => console.log(data));
-keepingPromise.catch((data) => console.log(data));
+keepingPromise()
+  .then((data) => console.log(data))
+  .catch((data) => console.log(data));

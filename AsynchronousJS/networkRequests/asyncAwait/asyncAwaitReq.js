9d3adb6646933bd6b4421a2e45047529ecde7e41
mpilopillz <mpilo.dlamini@yahoo.com>
2021-07-11 15:00:05 +0200

    wrapping getplanets in try catch

--- AsynchronousJS/networkRequests/asyncAwait/asyncAwaitReq.js
+++ AsynchronousJS/networkRequests/asyncAwait/asyncAwaitReq.js
@@ -1,3 +1,15 @@
+async function getData() {
+  try {
+    const data = await axios.get("https://swapi.dev/api/planets/");
+    console.log(data);
+    console.log(data);
+    return data;
+  } catch (e) {
+    console.log(`error caught -->`, e);
+  }
+}
+
+getData().then((data) => console.log(data));
 function getData() {
   const data = axios.get("https://swapi.dev/api/planets/").then((data) => {
     console.log(data);
@@ -5,13 +17,4 @@ function getData() {
   console.log(data);
   return data;
 }
-
 getData().then((data) => console.log(data));
-// function getData() {
-//   const data = axios.get("https://swapi.dev/api/planets/").then((data) => {
-//     console.log(data);
-//   });
-//   console.log(data);
-//   return data;
-// }
-// getData().then((data) => console.log(data));

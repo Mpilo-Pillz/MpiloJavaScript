5a14f26785f5fae6d989d1933099f240285a6a1b
Fikasentani <mpilo.dlamini@yahoo.com>
2021-07-12 08:16:33 +0200

    looking at pending and resolved state

--- AsynchronousJS/sequentialVSPArallel/sequentialParallell.js
+++ AsynchronousJS/sequentialVSPArallel/sequentialParallell.js
@@ -23,6 +23,23 @@ async function get3ParalellUndefinedPokemon() {
     console.log(poke3.data);
 }
 
+async function get3ParalellPokemon() {
+    const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/1")
+    const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/2")
+    const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/3")
 
+    console.log(prom1) //prints pending state
+    const poke1 = await prom1
+    const poke2 = await prom2
+    const poke3 = await prom3
+    console.log(prom1); //prints resolved state
+
+    // works
+    console.log(poke1.data);
+    console.log(poke2.data);
+    console.log(poke3.data);
+}
+
+get3ParalellPokemon()
 get3ParalellUndefinedPokemon()
 get3SequentialPokemon();

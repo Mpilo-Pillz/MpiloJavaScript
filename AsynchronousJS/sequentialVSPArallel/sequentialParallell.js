b5cbd296ebd3d000b8e5273fb4860c66a76ad2a6
Fikasentani <mpilo.dlamini@yahoo.com>
2021-07-12 08:15:49 +0200

    not resolving printing undefined

--- AsynchronousJS/sequentialVSPArallel/sequentialParallell.js
+++ AsynchronousJS/sequentialVSPArallel/sequentialParallell.js
@@ -8,4 +8,21 @@ async function get3SequentialPokemon() {
     console.log(poke2.data);
 }
 
+async function get3ParalellUndefinedPokemon() {
+    const poke1 = axios.get("https://pokeapi.co/api/v2/pokemon/1")
+    const poke2 = axios.get("https://pokeapi.co/api/v2/pokemon/2")
+    const poke3 = axios.get("https://pokeapi.co/api/v2/pokemon/3")
+
+    await poke1
+    await poke2
+    await poke3
+
+    // undefined prints before resolution
+    console.log(poke1.data);
+    console.log(poke2.data);
+    console.log(poke3.data);
+}
+
+
+get3ParalellUndefinedPokemon()
 get3SequentialPokemon();

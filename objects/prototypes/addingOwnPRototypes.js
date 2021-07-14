be769ec988c14c38ecd0c06ee51c01ce69f5f6b6
mpilopillz <mpilo.dlamini@yahoo.com>
2021-07-14 05:11:20 +0200

    EXPLORING PROTOTYPES

---
+++ objects/prototypes/addingOwnPRototypes.js
@@ -0,0 +1,8 @@
+String.prototype.yell = function () {
+  return `COME ON!!! ${this.toLocaleUpperCase()}!!!!! YOU CAN DO IT!`;
+};
+
+// REPLACING EXISTIGN ONES
+Array.prototype.pop = function () {
+  return "SORRY I WONT LET YOU REMOVE THAT ELEMENT!";
+};

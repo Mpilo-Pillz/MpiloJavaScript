629770d5cfc17d997d2ba61aa522f8360c54de56
mpilopillz <mpilo.dlamini@yahoo.com>
2021-07-15 06:30:40 +0200

    an into to oop

---
+++ objects/prototypes/exploringOOP.js
@@ -0,0 +1,14 @@
+const navColor = new Color("carrot", [230, 126, 34]);
+const logoColor = new Color("emerald", [46, 204, 113]);
+
+console.log(navColor.rgb());
+console.log(navColor.rgba());
+console.log(navColor.hex());
+
+document.querySelectorAll("h1")[0].style.backgroundColor = navColor.rgb()(
+  // explore this video an intro to oop
+console.dir(document.querySelectorAll("h1"))
+  ("rgb(244,54,23)");
+("rgba(244, 54, 23,0.9)");
+("#76FF32");
+("hsl(180, 50%, 30%)"); // note that to get the opposite add 180 to teh hue (h) in this case make teh value 360

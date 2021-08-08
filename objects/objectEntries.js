ce5388f723b3fe429a61e228925a5f87f71ad412
Fikasentani <mpilo.dlamini@yahoo.com>
2021-08-08 12:00:38 +0200

    looping with obj entries

--- objects/objectEntries.js
+++ objects/objectEntries.js
@@ -4,3 +4,10 @@ const myObj = {
 }
 
 console.log(Object.entries(myObj));
+
+for (const skiya of Object.entries(myObj)) {
+    console.log(`${skiya}`);
+}
+for (const [key, value] of Object.entries(myObj)) {
+    console.log(`${key}: ${value}`);
+}

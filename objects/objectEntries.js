6c6f2f997bdfeb35a7e3c5230983c10ed8097c20
Fikasentani <mpilo.dlamini@yahoo.com>
2021-08-08 12:05:15 +0200

    looking at destrucuting

--- objects/objectEntries.js
+++ objects/objectEntries.js
@@ -3,11 +3,19 @@ const myObj = {
     lastName: 'mub'
 }
 
-console.log(Object.entries(myObj));
+const myArr = ['el1', 'el2', 'el3', 'el4', 'el5'];
 
-for (const skiya of Object.entries(myObj)) {
-    console.log(`${skiya}`);
+for (const elem of myArr) {
+    console.log(`${elem}`);
 }
-for (const [key, value] of Object.entries(myObj)) {
-    console.log(`${key}: ${value}`);
+for (const [elem, ent, s] of myArr) {
+    console.log(`${elem}:${ent}:${s}`);
 }
+// console.log(Object.entries(myObj));
+
+// for (const skiya of Object.entries(myObj)) {
+//     console.log(`${skiya}`);
+// }
+// for (const [key, value] of Object.entries(myObj)) {
+//     console.log(`${key}: ${value}`);
+// }

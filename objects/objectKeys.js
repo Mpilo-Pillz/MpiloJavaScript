731da195fcf3cf792ab493636ffe645dd8bfbf1b
Fikasentani <mpilo.dlamini@yahoo.com>
2021-08-09 22:07:36 +0200

    adding values for later comparison

--- objects/objectKeys.js
+++ objects/objectKeys.js
@@ -9,17 +9,30 @@ const myNestedObj = {
     },
 }
 const myItararr = []
-for (const [key, value] in Object.keys(myNestedObj)) {
-    // console.log('--', key);
-    // console.log(myNestedObj[value]);
+for (const key in Object.values(myNestedObj)) {
+    console.log('--', key);
+    // console.log(myNestedObj[key]);
     // myItararr.push(myNestedObj[value])
 }
 
 // console.log(Object.keys(myNestedObj));
 // console.log(Object.entries(myNestedObj));
+console.log("keys->", Object.keys(myNestedObj));
+console.log("val->", Object.values(myNestedObj));
 // console.log(myItararr);
 // console.log(myNestedObj['person']);
 
-const neArr = Object.keys(myNestedObj).map(key =>
+
+
+const neArr = Object.keys(myNestedObj).map(key => {
+    console.log(key);
     console.log(myNestedObj[key])
+    return myNestedObj[key]
+}
 )
+console.log(neArr);
+
+
+// const display = Object.keys(posts).map(key =>
+//     <option value={key}>{posts[key]}</option>
+//   )

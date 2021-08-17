0d6b35bc438611ee19cea8dcc76925b1fd68f743
mpilopillz <mpilo.dlamini@yahoo.com>
2021-08-17 08:33:20 +0200

    setting up timer class instance

--- ConceptProjecImplementation/ColtNGrider/TimerCountdwn/index.js
+++ ConceptProjecImplementation/ColtNGrider/TimerCountdwn/index.js
@@ -1,1 +1,18 @@
-console.log("WORKING!!");
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.startButton.addEventListener("click", this.start);
+  }
+
+  start() {
+    console.log("Starts timer");
+  }
+}
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.getElementById("start");
+const pauseButton = document.getElementById("pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);

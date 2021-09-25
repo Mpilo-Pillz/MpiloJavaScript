269231be4ea2a2f69367000afe81a4d1957022ce
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-25 13:57:53 +0200

    adding adding my version of sum numbers recursion

---
+++ ConceptPractice/recursion/sumRange.js
@@ -0,0 +1,26 @@
+function sumRange(n) {
+    // debugger
+    let total = 0;
+
+    for (let i = n; i > 0; i--) {
+        // total += n; // super interesting mistke I made
+        total += i;
+    }
+
+    return total
+}
+
+// console.log("sumtange-->", sumRange(3))
+
+function recursiveSumRange(n, total = 0) {
+    total += n
+
+    if (n === 0) {
+        return total
+    }
+
+    return recursiveSumRange(n - 1, total)
+
+}
+
+console.log("recursive-->", recursiveSumRange(4));

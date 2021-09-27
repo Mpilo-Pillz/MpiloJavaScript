81b41df07ae1e67e3569e8181e653d8bcb1271b4
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-27 05:23:55 +0200

    adding a count up recursive version of fizzbuzz

--- ConceptPractice/recursion/fizzBuzzRecursion.js
+++ ConceptPractice/recursion/fizzBuzzRecursion.js
@@ -48,4 +48,36 @@ function fizzBuzzREcursive(num) {
     fizzBuzzREcursive(num - 1)
 }
 
-fizzBuzzREcursive(200)
+// fizzBuzzREcursive(200)
+
+const fizzBuzzRecursiveCountUp = (num, end) => {
+    const divisibleByFive = num % 5 === 0;
+    const divisibleByThree = num % 3 === 0;
+    const divisibleByFiveAndThree = divisibleByThree && divisibleByFive;
+
+    if (num <= 0) {
+        console.log("ending on -->", num);
+        return
+    }
+
+
+
+
+    if (divisibleByFiveAndThree) {
+        console.log("Fizzbuzz");
+    }
+    else if (divisibleByThree) {
+        console.log("Fizz");
+    }
+    else if (divisibleByFive) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+
+    if (num < end) {
+        fizzBuzzRecursiveCountUp(num + 1, end)
+    }
+}
+
+fizzBuzzRecursiveCountUp(1, 300)

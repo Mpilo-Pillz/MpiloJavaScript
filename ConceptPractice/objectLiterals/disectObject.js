ec0127a2c7fb646cfc8417a8230e070857afd210
Fikasentani <mpilo.dlamini@yahoo.com>
2021-10-02 09:56:58 +0200

    disecting object keys and values

---
+++ ConceptPractice/objectLiterals/disectObject.js
@@ -0,0 +1,13 @@
+const myObj = {
+    firstName: "Mpilo",
+    lastName: "Dlamini",
+    companyName: "Fikasentech"
+}
+
+// ** Object.Keys
+/** Puts keys into an array*/
+// console.log("ObjKeys-->", Object.keys(myObj))
+
+//  ** Object.values
+// * puts values into an array
+console.log("ObjValues-->", Object.values(myObj))

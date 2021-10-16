2eebdbf1cd7b1c2af5620a2834137612536d02db
Fikasentani <mpilo.dlamini@yahoo.com>
2021-10-16 08:56:19 +0200

    looking at primitive pointers

---
+++ weirdStuff/pointerVsReference.js
@@ -0,0 +1,9 @@
+Primitives
+
+let myName = "Fikasentani";
+let secondName = myName; // Fikasentani it copies the value
+myName = "Thulani";
+
+console.log("2nd Name", secondName);
+console.log("myName", myName);
+

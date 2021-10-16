9de7fed39a7e26442b7531e33b8b43126b392009
Fikasentani <mpilo.dlamini@yahoo.com>
2021-10-16 09:03:47 +0200

    looking at reference type

--- weirdStuff/pointerVsReference.js
+++ weirdStuff/pointerVsReference.js
@@ -1,4 +1,4 @@
-Primitives
+// Primitives
 
 let myName = "Fikasentani";
 let secondName = myName; // Fikasentani it copies the value
@@ -6,4 +6,19 @@ myName = "Thulani";
 
 console.log("2nd Name", secondName);
 console.log("myName", myName);
+
+// Referance Values
+
+const engineer = {
+    name: "Mpilo",
+    age: '29',
+    languages: ['JavaScript', 'TypeScript']
+}
+console.log(engineer);
+
+const secondEngineer = engineer;
+console.log(secondEngineer);
+
+engineer.name = 'Pillz';
+console.log('mutated-->', secondEngineer);
 

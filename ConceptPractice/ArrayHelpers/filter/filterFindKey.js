4766bf0aedf40840e4853dbd62fd32c5d5d3c887
mpilopillz <mpilo.dlamini@yahoo.com>
2021-11-10 06:04:44 +0200

    looking an array dot filter

---
+++ ConceptPractice/ArrayHelpers/filter/filterFindKey.js
@@ -0,0 +1,51 @@
+const banks = [
+    {
+      key: 1,
+      label: "JavaScript",
+    },
+    {
+      key: 2,
+      label: "TypeScript",
+    },
+    {
+      key: 3,
+      label: "Python",
+    },
+    {
+      key: 4,
+      label: "Swift",
+    },
+    {
+      key: 5,
+      label: "Dart",
+    },
+    {
+      key: 6,
+      label: "Java",
+    },
+    {
+      key: 7,
+      label: "C#",
+    },
+    {
+      key: 8,
+      label: "HTML",
+    },
+    {
+      key: 9,
+      label: "CSS",
+    },
+    {
+      key: 10,
+      label: "Swift",
+    },
+    
+  ];
+
+ 
+function pillz(key) {
+    return banks.find(bank => bank.key === key).label
+}
+
+console.log(pillz(1))
+console.log(pillz(10))

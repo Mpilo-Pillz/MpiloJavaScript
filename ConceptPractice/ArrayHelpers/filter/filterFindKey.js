768f71a906e6420a7531c3f25ab73f68baeb1fc2
mpilopillz <mpilo.dlamini@yahoo.com>
2021-11-11 03:31:45 +0200

    using array find method

--- ConceptPractice/ArrayHelpers/filter/filterFindKey.js
+++ ConceptPractice/ArrayHelpers/filter/filterFindKey.js
@@ -47,5 +47,11 @@ function pillz(key) {
     return banks.find(bank => bank.key === key).label
 }
 
+function fika(key) {
+  return banks.filter(bank => bank.key === key).label
+}
+
 console.log(pillz(1))
 console.log(pillz(10))
+console.log(fika(10))
+console.log(fika(10))

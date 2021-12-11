731924e343e21ca84f952e5555a5ed8b6929d9dc
Fikasentani <mpilo.dlamini@yahoo.com>
2021-12-11 20:47:30 +0200

    learning computed properties to log to console

---
+++ protips/fromFireShip.js
@@ -0,0 +1,7 @@
+const mub = { name: "mub", age: "3" }
+const heleb = { gama: "heleb", age: "4" }
+const mpi = { gama: "mpi", age: "5" }
+
+// computed property names
+console.log("%c My debuggers", "color: green; font-weight: bold");
+console.log({ mub, heleb })

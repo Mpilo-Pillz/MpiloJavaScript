8b1e3fb4f124bf43c1b84876e39e1885cec84e5c
mpilopillz <mpilo.dlamini@yahoo.com>
2021-12-29 11:14:44 +0200

    wip - printing obscured word

--- ConceptProjecImplementation/hangman/hangman.js
+++ ConceptProjecImplementation/hangman/hangman.js
@@ -1,5 +1,7 @@
 const stages = require('./hangmanArt');
 const wordList = require('./hangmanWords')
-
-console.log(stages[0]);
-console.log(wordList[1]);
+const wordAnswerPosition = (Math.floor(Math.random() * wordList.length) - 1);
+const wordAnswer = wordList[wordAnswerPosition];
+console.log(`psst, the answer is ${wordAnswer}`);
+const obsurialArray = wordAnswer.split('').map(letter => '_')
+console.log("Please guess a letter in the word\n", obsurialArray );

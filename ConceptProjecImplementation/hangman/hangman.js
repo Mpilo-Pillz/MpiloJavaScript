bdd92975b711022512fb93f15c8fa9ef586bda63
mpilopillz <mpilo.dlamini@yahoo.com>
2021-12-30 12:13:42 +0200

    adding prompt library

--- ConceptProjecImplementation/hangman/hangman.js
+++ ConceptProjecImplementation/hangman/hangman.js
@@ -1,7 +1,19 @@
 const stages = require('./hangmanArt');
+const prompt = require('prompt');
 const wordList = require('./hangmanWords')
 const wordAnswerPosition = (Math.floor(Math.random() * wordList.length) - 1);
 const wordAnswer = wordList[wordAnswerPosition];
+const isGameOn = true;
 console.log(`psst, the answer is ${wordAnswer}`);
 const obsurialArray = wordAnswer.split('').map(letter => '_')
 console.log("Please guess a letter in the word\n", obsurialArray );
+
+prompt.start();
+
+prompt.get(['userguess'], function (err, result) {
+    console.log(result.userguess);
+})
+
+while (isGameOn) {
+    
+}

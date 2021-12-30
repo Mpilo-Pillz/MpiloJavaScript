451e905034d172d0dce816e0aaca313b57ae5bb2
Fikasentani <mpilo.dlamini@yahoo.com>
2021-12-30 13:51:51 +0200

    letting user know if correct word was guessed

--- ConceptProjecImplementation/hangman/hangman.js
+++ ConceptProjecImplementation/hangman/hangman.js
@@ -12,29 +12,38 @@ console.log("Please guess a letter in the word\n", obsurialArray);
 console.log(stages[livesLeft])
 while (isGameOn) {
     // Get user input
-    let guess = prompt('Guess a letter in the word: ');
+    let guess = prompt('Guess a letter in the word: ').toLowerCase();
+    console.clear();
 
+    if (obsurialArray.indexOf(guess) > -1) {
+        console.log(`YOU GUESSED ${guess} already! No lives lost`);
+    }
     for (let i = 0; i < wordAnswer.length; i++) {
         if (guess === wordAnswer[i]) {
             obsurialArray[i] = guess
+            console.log("CORRECT!\n", stages[livesLeft]);
         }
     }
     if (wordAnswer.indexOf(guess) === -1) {
         livesLeft--;
-        console.log(stages[livesLeft]);
+        console.log("WRONG!\n", stages[livesLeft]);
     }
 
     console.log(obsurialArray)
 
-    if (obsurialArray.join('') === wordAnswer) {
+    // if (obsurialArray.join('') === wordAnswer) {
+    //     isGameOn = false;
+    //     console.log(`CONGRATULATIONS, YOU WIN!\n You guessed the word correctly. The word was ${obsurialArray.join('')}`)
+    // }
+
+    if (obsurialArray.indexOf('_') === -1) {
         isGameOn = false;
-        console.log(`CONGRATULATIONS, YOU WIN!\n You guessed the word correctly. The word was ${obsurialArray}`)
+        console.log(`CONGRATULATIONS, YOU WIN!\n You guessed the word correctly. The word was ${obsurialArray.join('')}`)
     }
 
     if (livesLeft === 0) {
         console.log(`GAME OVER, YOU LOOSE!.\n The word was ${wordAnswer}`);
         isGameOn = false;
     }
-
 
 }

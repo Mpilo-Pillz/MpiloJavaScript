a1594ace3fa43623afd6957f2e2f64f62e50eca0
Fikasentani <mpilo.dlamini@yahoo.com>
2021-12-30 13:19:58 +0200

    reducing lives and showing art to reflect loss of lives

--- ConceptProjecImplementation/hangman/hangman.js
+++ ConceptProjecImplementation/hangman/hangman.js
@@ -4,10 +4,12 @@ const wordList = require('./hangmanWords')
 const wordAnswerPosition = (Math.floor(Math.random() * wordList.length) - 1);
 const wordAnswer = wordList[wordAnswerPosition];
 const isGameOn = true;
+let livesLeft = stages.length - 1
 console.log(`psst, the answer is ${wordAnswer}`);
 const obsurialArray = wordAnswer.split('').map(letter => '_')
 console.log("Please guess a letter in the word\n", obsurialArray);
 
+console.log(stages[livesLeft])
 while (isGameOn) {
     // Get user input
     let guess = prompt('Guess a letter in the word: ');
@@ -16,6 +18,10 @@ while (isGameOn) {
         if (guess === wordAnswer[i]) {
             obsurialArray[i] = guess
         }
+    }
+    if (wordAnswer.indexOf(guess) === -1) {
+        livesLeft--;
+        console.log(stages[livesLeft]);
     }
     console.log(obsurialArray)
 

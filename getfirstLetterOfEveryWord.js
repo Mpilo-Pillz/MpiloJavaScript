8443aaa6e99eb727027fbef5d63e40534e00a2f8
mpilopillz <mpilo.dlamini@yahoo.com>
2022-12-13 23:54:45 +0200

    gettng first letter of every word and capitalizing it

---
+++ getfirstLetterOfEveryWord.js
@@ -0,0 +1,7 @@
+const one = "Mpilo".split(" ").map(letter => letter[0].toUpperCase())
+const two = "Mpilo Pillz".split(" ").map(letter => letter[0].toUpperCase())
+const three = "Mpilo Pillz Mpilz".split(" ").map(letter => letter[0].toUpperCase())
+
+console.log(one);
+console.log(two);
+console.log(three);

9f9f36aff35fada4d3d92c05972b661a110c9e4f
mpilopillz <mpilo.dlamini@yahoo.com>
2023-06-16 12:12:45 +0200

    learning logical assignment

--- ConceptProjecImplementation/logicalAssignment.js
+++ ConceptProjecImplementation/logicalAssignment.js
@@ -7,8 +7,8 @@ const ufcChampions = {
 
 }
 
-console.log(`The Heavyweight champion is ${ufcChampions.heavyweight ||= "currently vacant"}`)
-console.log(`The Heavyweight champion is ${ufcChampions.heavyweight || "currently vacant"}`)
+console.log(`The Heavyweight champion is ${ufcChampions.heavyweight ||= "currently vacant"}`) // mutatess the object
+console.log(`The Heavyweight champion is ${ufcChampions.heavyweight || "currently vacant"}`) // does not mutate the object
 console.log(`The Straweight champion is ${ufcChampions.strawWeight ||= "currently vacant"}`)
 console.log(`The Featherweight champion is ${ufcChampions.mFeatherweight ||= "currently vacant"}`)
 console.log(`The Women's Flyweight champion is ${ufcChampions.wFlyweight ||= "currently vacant"}`)

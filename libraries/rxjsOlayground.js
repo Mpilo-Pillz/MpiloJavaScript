5329846c31579e7f2d6a6e67ce7da6df89b1d162
mpilopillz <mpilo.dlamini@yahoo.com>
2023-06-23 12:12:45 +0200

    code form online visualizer

--- libraries/rxjsOlayground.js
+++ libraries/rxjsOlayground.js
@@ -8,4 +8,14 @@
 
 
 // https://out.stegrider.vercel.app/
+// the below only works on the website above
+const { fromEvent } = Rx;
 
+const input = document.createElement('input');
+const container = document.querySelector('.container');
+container.appendChild(input)
+
+const observable = fromEvent(input, 'input')
+
+// This is specific to this tool
+observable;

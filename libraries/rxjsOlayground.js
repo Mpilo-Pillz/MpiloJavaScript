0d46efa9fee5376ea6fee0b69ff5e0a723cd1615
mpilopillz <mpilo.dlamini@yahoo.com>
2023-06-24 12:12:45 +0200

    learning map operator

--- libraries/rxjsOlayground.js
+++ libraries/rxjsOlayground.js
@@ -10,12 +10,18 @@
 // https://out.stegrider.vercel.app/
 // the below only works on the website above
 const { fromEvent } = Rx;
+const { map } = RxOperators;
 
 const input = document.createElement('input');
 const container = document.querySelector('.container');
 container.appendChild(input)
 
-const observable = fromEvent(input, 'input')
+const observable = fromEvent(input, 'input').pipe(
+map(event =>event.target.value)
+map(value => console.log(value))
+)
+
+
 
 // This is specific to this tool
 observable;

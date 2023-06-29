0c11c1338bc8df9473fe5eed6b6391f07bae30df
Fikasentani <mpilo.dlamini@yahoo.com>
2023-06-29 23:11:22 +0200

    getting just the names and saving them in an arry

---
+++ gymDojo/formatters/objectToArray.js
@@ -0,0 +1,5 @@
+const measurementUnit = [{ name: "kg", unit: 1 }, { name: "Punds lb", unit: 10 }]
+
+const arrayOfUnitNames = measurementUnit.map((unit) => unit.name)
+
+console.log(arrayOfUnitNames);

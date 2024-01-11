9d096a850c1c57bb4e934ee4c804851a574d2423
Fikasentani <mpilo.dlamini@yahoo.com>
2024-01-11 18:22:40 +0200

    summing from arrays

--- gymDojo/optimizing/sumSquares.js
+++ gymDojo/optimizing/sumSquares.js
@@ -7,4 +7,15 @@ function sumSquares(num) {
     return sum
 }
 
+
+
+function sumSquaresFromArray(numbers) {
+    let sum = 0;
+    numbers.forEach(num => {
+        sum += num * num
+    });
+    return sum
+}
+
 console.log(sumSquares(3));
+console.log(sumSquaresFromArray([1, 2, 3]));

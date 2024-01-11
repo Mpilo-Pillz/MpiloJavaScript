ff6099f745ac214428f0998a2edfda975610ec02
Fikasentani <mpilo.dlamini@yahoo.com>
2024-01-11 18:27:33 +0200

    splitting out squaring from summing

--- gymDojo/optimizing/sumSquares.js
+++ gymDojo/optimizing/sumSquares.js
@@ -17,5 +17,13 @@ function sumSquaresFromArray(numbers) {
     return sum
 }
 
+function sumSquaresFromArrayOptimized(nums) {
+    const squares = nums.map(num => num * num)
+    const sum = squares.reduce((accumilator, currentVal) => accumilator + currentVal, 0)
+
+    return sum
+}
+
 console.log(sumSquares(3));
 console.log(sumSquaresFromArray([1, 2, 3]));
+console.log(sumSquaresFromArrayOptimized([1, 2, 3]));

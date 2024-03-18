6e95a342171572f98b4365f5f729bedef4f53783
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-18 20:50:45 +0200

    using some as a conditinal checker

---
+++ someConditionFromPhiwe.js
@@ -0,0 +1,3 @@
+const activities = ["Adventure Cycle", "Adventure Swimming", "Running", "Wight Lift"];
+
+activities.some((activity) => activity.indexOf("Adventure") > -1) ? console.log("I am Adevnturous") : console.log("NOT ADVENTUROUS")

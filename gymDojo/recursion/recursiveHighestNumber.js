bbc5c7cf47ffd7b8571b6df934467452f3edec08
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-30 23:11:22 +0200

    recursion higest number

---
+++ gymDojo/recursion/recursiveHighestNumber.js
@@ -0,0 +1,17 @@
+function highestNumber() {
+    let highestNumber = 0;
+}
+
+function loopThoughArray(arr) {
+    let index = arr.length - 1
+    let stepper = 0
+
+    if (index >= 0) {
+        stepper++
+    }
+}
+
+loopThoughArray([1, 5, 2, 4, 3])
+
+highestNumber([1, 5, 2, 4, 3])
+

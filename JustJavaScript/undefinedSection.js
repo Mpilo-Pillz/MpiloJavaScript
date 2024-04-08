c70e970b6838016c4e8f3d25e0457abff556be20
mpilopillz <mpilo.dlamini@yahoo.com>
2024-04-08 20:57:27 +0200

    learning null nd undefined

---
+++ JustJavaScript/undefinedSection.js
@@ -0,0 +1,9 @@
+// unintentional missing value
+let oilerPlate = undefined;
+console.log(oilerPlate); // undefined
+console.log(typeof undefined); // undefined
+console.log(nonExistant); // ReferenceError: nonExistant is not defined has nothing to do with undefined
+
+// Null is an inential missing value
+// Null is an object which is a lie casued by a but as it is primitive
+console.log(typeof null); // object

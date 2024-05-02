0e7042d1987ceabcf49dc7c6bc83384356d3a03e
Fikasentani <mpilo.dlamini@yahoo.com>
2024-05-02 21:50:14 +0200

    leanring functional programming

--- 2024Revision/CallStack/shapeCalculator.js
+++ 2024Revision/CallStack/shapeCalculator.js
@@ -2,4 +2,8 @@ const multiply = (x, y) => x * y;
 
 const square = (x) => multiply(x, x);
 
+isRightTriabgle = (a, b, c) => square(a) + square(b) === square(c)
+
 console.log(square(5));
+console.log(isRightTriabgle(5, 4, 3));
+

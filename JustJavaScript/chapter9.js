2e4aec5f0700ecd308fa782b0d48bbdc87c4f8bc
Fikasentani <mpilo.dlamini@yahoo.com>
2024-05-10 00:02:49 +0200

    learning(chapter 9): looking at how objects behave with let and const

--- JustJavaScript/chapter9.js
+++ JustJavaScript/chapter9.js
@@ -20,3 +20,11 @@ console.log(sherlock.surname); // "Holmes"
 console.log("Watch this------>", sherlock.address.city); // "Malibu" // corrected by replacing line 12 with line 13 or line 14
 console.log(john.surname); // "Lennon"
 console.log("NAd this -->", john.address.city) // "Malubi"
+
+// let vs const for objects aka reference variiables
+const shrek = { speciies: 'ogre' }
+let fiona; // Changes the error on line 27 from Reference Error to Type Error
+// shrek = fiona; // Reference Error without let fiona 
+
+shrek.speciies = 'human';
+console.log(shrek.speciies); // 'human'

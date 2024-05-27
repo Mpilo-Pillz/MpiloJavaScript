b6df743716ee2db902dc07d65e860eb1f66240f0
Fikasentani <mpilo.dlamini@yahoo.com>
2024-05-27 22:04:17 +0200

    chapter 10 quiz butter bitter

--- JustJavaScript/chapter10/quiz10.js
+++ JustJavaScript/chapter10/quiz10.js
@@ -12,3 +12,16 @@ console.log("lie own prop", cake.hasOwnProperty('taste')); // false
 console.log("lie own prop", lie.hasOwnProperty('taste')); // true
 console.log("cake taste same as lie taste", cake.taste === lie.taste); // true
 console.log(cake.hasOwnProperty('taste') === lie.hasOwnProperty('taste'));// false
+
+// let otherLie = {
+//    taste: 'bitter' 
+// }
+
+// let otherCake = {
+//     __proto__: lie
+// }
+
+lie.taste = "butter"
+console.log(cake.taste)
+
+

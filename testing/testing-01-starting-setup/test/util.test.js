05778b10e99f514d73d5c0936fce0d6aeed5334f
Fikasentani <mpilo.dlamini@yahoo.com>
2024-07-18 22:37:08 +0200

    negative testing

--- testing/testing-01-starting-setup/test/util.test.js
+++ testing/testing-01-starting-setup/test/util.test.js
@@ -4,3 +4,8 @@ test('should output name and age', () => {
     const text = generateText("Botshelo", 31)
     expect(text).toBe("Botshelo (31 years old)")
 });
+
+test('should output data-less text', () => {
+    const text = generateText('', null)
+    expect(text).toBe(" (null years old)")
+})

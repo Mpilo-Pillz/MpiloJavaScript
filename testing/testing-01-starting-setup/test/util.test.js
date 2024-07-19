73cd789c10e5075bb476439b0b95a5dcf4ac22cc
Fikasentani <mpilo.dlamini@yahoo.com>
2024-07-19 22:37:08 +0200

    further avoiding fals positive test

--- testing/testing-01-starting-setup/test/util.test.js
+++ testing/testing-01-starting-setup/test/util.test.js
@@ -3,6 +3,9 @@ const { generateText } = require('../util');
 test('should output name and age', () => {
     const text = generateText("Botshelo", 31)
     expect(text).toBe("Botshelo (31 years old)")
+
+    const text2 = generateText("Mphendvulo", 30)
+    expect(text2).toBe("Mphendvulo (30 years old)")
 });
 
 test('should output data-less text', () => {
